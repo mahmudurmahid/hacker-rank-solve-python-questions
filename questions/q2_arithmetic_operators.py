ad74e74157830d1486a59793f088212403f40b86
mahmudurmahid <m.mahid@icloud.com>
2025-05-28 12:49:49 +0100

    solution for q2

---
+++ questions/q2_arithmetic_operators.py
@@ -0,0 +1,17 @@
+def arithmetic_operation(a, b):
+    addition = a + b
+    subtraction = a - b
+    multiplication = a * b
+
+    print(addition)
+    print(subtraction)
+    print(multiplication)
+
+a = 3
+b = 5
+result = arithmetic_operation(a, b)
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     arithmetic_operation(a, b)
