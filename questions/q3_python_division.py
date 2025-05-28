297a2a4ef8ff79455a0e4bc185785d1e03ac40a6
mahmudurmahid <m.mahid@icloud.com>
2025-05-28 12:53:18 +0100

    solution for q3

---
+++ questions/q3_python_division.py
@@ -0,0 +1,15 @@
+def division(a, b):
+    int_division = a // b
+    float_division = a / b
+
+    print(int_division)
+    print(float_division)
+
+a = 3
+b = 5
+result = division(a, b)
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     division(a, b)
