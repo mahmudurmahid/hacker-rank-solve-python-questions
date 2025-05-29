30d00577a9a267c23167bae7b605d75ce45a43ac
mahmudurmahid <m.mahid@icloud.com>
2025-05-29 10:25:35 +0100

    solved q9

--- questions/q9_nested_lists.py
+++ questions/q9_nested_lists.py
@@ -1,15 +1,14 @@
 def student_grades(arr_list):
     # highest to lowest list of grades & unique scores from this list
     ascending_scores = sorted(arr_list, key=lambda x: x[1], reverse=True)
-    unique_scores = sorted(set([score for name, score in ascending_scores]))
+    unique_scores = sorted(set([score for name, score in ascending_scores]), reverse=True)
 
     # find the lowest grade and second lowest grade
     lowest_grade = ascending_scores[-1][-1]
-    second_lowest_grade = unique_scores[1]
+    second_lowest_grade = unique_scores[-2]
     
     # find names of students with the second lowest grade
     second_lowest_grade_students = [name for name, score in ascending_scores if score == second_lowest_grade]
-    print(second_lowest_grade_students)
 
     names = []
     # sort the names alphabetically into names list
