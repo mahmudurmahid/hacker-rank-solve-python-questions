e5b3f368ecab9e898b166f7ef33cde29152b9071
mahmudurmahid <m.mahid@icloud.com>
2025-05-29 14:35:09 +0100

    solved q15

---
+++ questions/q15_whats_your_name.py
@@ -0,0 +1,7 @@
+def greeting(first_name, last_name):
+    return f"Hello {first_name} {last_name}! You just delved into python."
+
+first_name = "Ross"
+last_name = "Taylor"
+result = greeting(first_name, last_name)
+print(result)
