aff3e59217f0ec6cc93c431e14f45ff648aef6fa
mahmudurmahid <m.mahid@icloud.com>
2025-09-22 16:18:16 +0100

    alt solution for q25

--- questions/q21-30/q25_capitalize.py
+++ questions/q21-30/q25_capitalize.py
@@ -3,6 +3,19 @@ def solve(s):
 
     return capitalized_name
 
+
+def alt_solve(s):
+    capitalized_name = s.split(" ")
+
+    new_name = ""
+
+    for name in capitalized_name:
+        new_name += name.capitalize() + " "
+    
+    return new_name
+
 s = "chris alan"
 result = solve(s)
 print(result)
+alt_result = alt_solve(s)
+print(alt_result)
